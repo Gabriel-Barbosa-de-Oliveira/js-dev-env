cb800af2042449bcf4db5df6fc45272c022e73ab
Gabriel Barbosa <gabrieljoimano@hotmail.com>
2018-07-19 20:40:49 -0300

    Es lint

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,3 @@
 import chalk from "chalk";
 //Chalk seta a cor da mensagem no console
-console.log(chalk.green("Starting app in dev node.."));
+console.log(chalk.green("Starting app in dev node..")); //eslint-disable-line no-console
